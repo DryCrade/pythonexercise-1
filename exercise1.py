421e716bb8a1b0a268f09ab09a76cd659f420352
DryCrade <144786409+DryCrade@users.noreply.github.com>
2023-09-25 13:07:31 +0700

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,26 @@
+# #1
+# print("Enter your height")
+# feet = int(input("Feet: "))
+# inches = int(input("Inches: "))
+
+# feetcm = feet * 30.48
+# inchescm = inches * 2.54
+# totalcm = feetcm + inchescm
+# boardcm = totalcm*(88/100)
+
+# print(f'Suggested board length = {boardcm}')
+
+# #2
+# force,mass = input("Enter force in Newtons and mass in kg: ").split(",")
+# acc = float(force)/float(mass)
+# print(f'Acceleration = {acc} m/s^2')
+
+#3
+verb = input("Enter a verb: ")
+adj = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+
+print(f'A shinigami {verb} my {adj} {noun1} out of the {noun2} as if he were a vegetarian fishing a {noun3} out of his salad.')
+
